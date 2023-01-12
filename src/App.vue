70a14aeb08f1d1b9c937c0db75607bc3f50ba099
zhangzhibang <295854713@qq.com>
2023-01-13 04:17:55 +0800

    feat

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <h1>笨蛋火火</h1>
+  <button @click="foo">btn</button>
+  <p>{{ msg }}</p>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue';
 
-export default {
-  name: 'App',
+let msg = ref("hhh");
+async function foo() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await res.json()
+  msg.value = data
 }
 </script>
